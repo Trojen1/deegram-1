021339b13629c9fe811d6cbf32d056eb0160b27d
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-12-04 15:44:37 +0530

    Update inline.py

--- deegram/plugins/inline.py
+++ deegram/plugins/inline.py
@@ -38,7 +38,10 @@ async def inline(event: InlineQuery.Event):
                 ),
             )
 
-    elif len((event.text.replace(".t", "").strip()) > 1:
+    else event.text.startswith(".t "):
+        track = event.text.replace(".t", "").strip()
+        if len(track) < 1:
+            return
         logger.debug(f'🥰Searching for track: {event.text}')
         api_search_link = "https://api.deezer.com/search?q=" + event.text
 
